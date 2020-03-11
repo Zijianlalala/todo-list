6c7ae868a7a82b5cb1c15a2586f548f500fe2be6
zijianwu <zijian_wu@foxmail.com>
2020-03-12 00:24:35 +0800

    使用了localStorage进行缓存

--- js/todolist.js
+++ js/todolist.js
@@ -5,65 +5,75 @@ function prepareList() {
     var aLi = oUl.getElementsByTagName('li');
     // 新建项
     aBtn[0].onclick = function () {
+        newItem(null, true);
+    }
+    // 删除选中项
+    aBtn[1].onclick = function () {
+        var length = aLi.length;
+        var index = localStorage.getItem('index');
+        var aIndex = index.split(',');
+        for (var i = 0; i < aLi.length; i++) {
+            if (aLi[i].getElementsByTagName('input')[0].checked) {
+                var deleteValue = aLi[i].getElementsByTagName('input')[1].value;
+                var po = aIndex.indexOf(deleteValue);
+                aIndex.splice(po, 1);
+                localStorage.setItem('index', aIndex.join(','));
+                // console.log(index);
+                aLi[i].parentElement.removeChild(aLi[i]);
+                i--;
+            }
+        }
+    }
+    // 新建列表项li
+    function newItem(value, focused) {
+        // 创建item
         var oCheckBox = document.createElement('input');
         oCheckBox.type = 'checkbox';
         var oText = document.createElement('input');
         oText.tyle = 'text';
         oText.className = 'txt';
         oText.placeholder = '请输入待办项';
-        // 设置完成点击事件
-        oCheckBox.onclick = function() {
-            lineThrough(oText);
-        }
-        // 设置键盘事件
-        oText.onkeyup = function(event) {
-            var event = event || window.event;
-            if (event.keyCode == 13) {
-                aBtn[0].onclick();
-                // localStorage.setItem('value')
-            }
-        }
+        value && (oText.value = value);
         // 绑定元素
         var oLi = document.createElement('li');
         oLi.appendChild(oCheckBox);
         oLi.appendChild(oText);
         oUl.appendChild(oLi);
-        // 为新的对话框设计焦点
-        oText.focus();
-    }
-    // 删除选中项
-    aBtn[1].onclick = function () {
-        var length = aLi.length;
-        for (var i = 0; i < aLi.length; i++) {
-            if (aLi[i].getElementsByTagName('input')[0].checked) {
-                aLi[i].parentElement.removeChild(aLi[i]);
-                i--;
+        // 设置完成点击事件
+        oCheckBox.onclick = function () {
+            var style = oText.style;
+            style.textDecoration = (style.textDecoration == 'line-through' ? 'none' : 'line-through');
+        }
+        // 设置键盘事件
+        oText.onkeyup = function (event) {
+            var event = event || window.event;
+            if (event.keyCode == 13) {
+                aBtn[0].onclick();
+                if (!localStorage.getItem('index')) {
+                    localStorage.setItem('index', '');
+                }
+                var index = localStorage.getItem('index');
+                if (this.value && index.indexOf(this.value) == -1) {
+                    index += index == '' ? this.value : ',' + this.value;
+                    localStorage.setItem('index', index);
+                    localStorage.setItem(this.value, this.value);
+                }
             }
         }
+        focused && oText.focus();
     }
-    // 完成项
-    // for (var i = 0; i < aLi.length; i++) {
-    //     var aInput = aLi[i].getElementsByTagName('input');
-    //     // 设置删除线 text-decoration-line: line-through;
-    //     aInput[0].onclick = function () {
-    //         lineThrough(aInput[1]);    
-    //     }
-        // aInput[1].onkeyup = function(event) {
-        //     var event = event || window.event;
-        //     newItem(event);
-        // }
-    // }
-    // 划删除线
-    function lineThrough(elem) {
-        var style = elem.style;
-        style.textDecoration = (style.textDecoration == 'line-through' ? 'none' : 'line-through');
+    // 启动页面
+    // 判断localStorage是否有缓存
+    if (localStorage.length > 0) {
+        var index = localStorage.getItem('index');
+        var aIndex = index.split(',');
+        for (var i = 0; i < aIndex.length; i++) {
+            var value = aIndex[i];
+            newItem(value, false);
+        }
+    } else {
+        aBtn[0].onclick();
     }
-    // input按回车键新建项并且将值保存到localStorage中
-    function newItem(event) {
-        // TODO 将焦点切换到新建的对话框中
-    }
-    //启动页面
-    aBtn[0].onclick();
 }
 
 window.onload = function () {
